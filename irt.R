4c33008055793bec786a96b03c31218856d9169c
Josh Cutler <josh.cutler@gmail.com>
2011-12-19 15:01:31 -0600

    Updated R script to use the "nextItem" method, fixes #5

--- irt.R
+++ irt.R
@@ -10,6 +10,7 @@ rook$add(
   app  = function(env) {
     req = Rook::Request$new(env)
     
+    #Construct the item bank
     discrimination = as.numeric(unlist(strsplit(req$params()$dis, ",")))
     difficulty = as.numeric(unlist(strsplit(req$params()$dif, ",")))
     guessing = as.numeric(unlist(strsplit(req$params()$g, ",")))
@@ -19,23 +20,25 @@ rook$add(
     questions = data.frame(a=discrimination, b=difficulty, c=guessing, d=inattention)
     items = createItemBank(items=questions)
  
+    #Figure out which questions have been answered
     answers = as.numeric(unlist(strsplit(req$params()$a, ",")))
     answered_questions = !is.na(answers)
     
-    theta_hat = eapEst(questions[answered_questions,], answers[answered_questions])
-    eapSem(theta_hat, questions[answered_questions,], answers[answered_questions])
- 
-    responses = c(0,1)
-    posterior_variance_estimates = {}
-    
-    for (i in seq(1, length(ids))) {
-      if (!answered_questions[i]) {
-        posterior_variance_estimates[[ids[i]]] = EPV(items, i, responses, theta_hat, questions[i,])
-      }
+    #Either take \hat{\theta} as a param or compute it
+    if("th" %in% names(req$params()))
+    {
+      theta_hat = as.numeric(req$params()$th)
     }
+    else
+    {
+      theta_hat = eapEst(questions[answered_questions,], answers[answered_questions])
+    }
+
+    #Compute the next item and return it
+    next_item = nextItem(items, theta_hat, criterion="MEPV")
     
     res = Rook::Response$new()
-    res$write(toJSON(posterior_variance_estimates))
+    res$write(toJSON(next_item))
     res$finish()
   }
 )
