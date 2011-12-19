d782883be3e0ba2fcdc2303f0f9973f17c85a7e1
Josh Cutler <josh.cutler@gmail.com>
2011-12-19 16:34:59 -0600

    Updated the output of the webservice
    Fixes #2

--- irt.R
+++ irt.R
@@ -31,22 +31,49 @@ rook$add(
     }
     else
     {
-      theta_hat = eapEst(questions[answered_questions,], answers[answered_questions])
+      theta_hat = thetaEst(questions[answered_questions,], answers[answered_questions], method="EAP")
     }
 
+    results = list()
+    results$theta_hat = theta_hat
+    
     #Compute the next item and return it
-    items = createItemBank(items=questions[unasked_questions,])
-    next_item = nextItem(items, theta_hat, criterion="MEPV")
-
+    items = createItemBank(items=questions)
+    next_item = nextItem(items, theta_hat, criterion="MEPV", out=as.numeric(rownames(questions[!unasked_questions,])))
+    
     #Convert item response to proper "ID"
-    next_id = questions[unasked_questions, "ids"][as.numeric(next_item["item"])]
+    next_id = questions[as.numeric(next_item["item"]),"ids"]
+    next_item_index = next_item["item"]$item
     next_item["item"] = next_id
+    
+    results$next_item = list()
+    results$next_item$item_id = next_item$item
+    results$next_item$EPV = next_item$info
+    results$next_item$criterion = next_item$criterion
 
+    #Compute some results to savea  web service call
+    question_index = as.numeric(rownames(questions[questions$ids == as.character(next_id),]))
+    hyp_answered_questions = answered_questions
+    hyp_answered_questions[question_index] = T
+    hyp_answers = answers
+    
+    #Compute values if item is correct
+    hyp_answers[question_index] = 1
+    hyp_theta_hat_correct = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
+    results$if_correct = list(theta_hat=hyp_theta_hat_correct)
+    
+    #Compute values if item is incorrect
+    hyp_answers[question_index] = 0
+    hyp_theta_hat_incorrect = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
+    results$if_incorrect = list(theta_hat=hyp_theta_hat_incorrect)
+    
     res = Rook::Response$new()
-    res$write(toJSON(next_item))
+    res$write(toJSON(results))
     res$finish()
   }
 )
+
+rook$remove(1)
 
 rook$start(listen='0.0.0.0', port=as.numeric(Sys.getenv("PORT")))
 
