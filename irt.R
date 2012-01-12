e333ff9af6057fcc3bc07597dd6401401bc50df0
Josh Cutler <josh.cutler@gmail.com>
2012-01-12 12:58:38 -0500

    Updated irt to take RO parameter

--- irt.R
+++ irt.R
@@ -11,6 +11,16 @@ rook$add(
   app  = function(env) {
     req = Rook::Request$new(env)
     
+    poly = F
+    if ("ro" %in% names(req$params())) 
+    {
+      ro = as.numeric(unlist(strsplit(req$params()$ro, ",")))
+      if (max(ro) > 2) 
+      {
+        poly = T  
+      }
+    }
+    
     #Construct the item bank
     discrimination = as.numeric(unlist(strsplit(req$params()$dis, ",")))
     difficulty = as.numeric(unlist(strsplit(req$params()$dif, ",")))
@@ -24,48 +34,56 @@ rook$add(
     unasked_questions = as.logical(as.numeric(unlist(strsplit(req$params()$uaq, ","))))
     #If an answer was NA it was not administered or they skipped it and we can't use it for computing theta
     answered_questions = !is.na(answers)
-
-    #Either take \hat{\theta} as a param or compute it
-    if("th" %in% names(req$params()))
+    
+    #Either take \hat{\theta} as a param or compute it    
+    if (poly)
     {
-      theta_hat = as.numeric(req$params()$th)
+      
     }
     else
     {
-      theta_hat = thetaEst(questions[answered_questions,], answers[answered_questions], method="EAP")
+      if("th" %in% names(req$params()))
+      {
+        theta_hat = as.numeric(req$params()$th)
+      }
+      else
+      {
+        theta_hat = thetaEst(questions[answered_questions,], answers[answered_questions], method="EAP")
+      }
+
+      #Compute the next item and return it
+      items = createItemBank(items=questions)
+      next_item = nextItem(items, theta_hat, criterion="MEPV", out=as.numeric(rownames(questions[!unasked_questions,])))
+    
+      #Convert item response to proper "ID"
+      next_id = questions[as.numeric(next_item["item"]),"ids"]
+      next_item_index = next_item["item"]$item
+      next_item["item"] = next_id
+      
+      #Compute some results to savea  web service call
+      question_index = as.numeric(rownames(questions[questions$ids == as.character(next_id),]))
+      hyp_answered_questions = answered_questions
+      hyp_answered_questions[question_index] = T
+      hyp_answers = answers
+      
+      #Compute values if item is correct
+      hyp_answers[question_index] = 1
+      hyp_theta_hat_correct = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
+    
+      #Compute values if item is incorrect
+      hyp_answers[question_index] = 0
+      hyp_theta_hat_incorrect = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
     }
 
     results = list()
     results$theta_hat = theta_hat
-    
-    #Compute the next item and return it
-    items = createItemBank(items=questions)
-    next_item = nextItem(items, theta_hat, criterion="MEPV", out=as.numeric(rownames(questions[!unasked_questions,])))
-    
-    #Convert item response to proper "ID"
-    next_id = questions[as.numeric(next_item["item"]),"ids"]
-    next_item_index = next_item["item"]$item
-    next_item["item"] = next_id
-    
+
     results$next_item = list()
     results$next_item$item_id = next_item$item
     results$next_item$EPV = next_item$info
     results$next_item$criterion = next_item$criterion
 
-    #Compute some results to savea  web service call
-    question_index = as.numeric(rownames(questions[questions$ids == as.character(next_id),]))
-    hyp_answered_questions = answered_questions
-    hyp_answered_questions[question_index] = T
-    hyp_answers = answers
-    
-    #Compute values if item is correct
-    hyp_answers[question_index] = 1
-    hyp_theta_hat_correct = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
     results$if_correct = list(theta_hat=hyp_theta_hat_correct)
-    
-    #Compute values if item is incorrect
-    hyp_answers[question_index] = 0
-    hyp_theta_hat_incorrect = thetaEst(questions[hyp_answered_questions,], hyp_answers[hyp_answered_questions], method="EAP")
     results$if_incorrect = list(theta_hat=hyp_theta_hat_incorrect)
     
     res = Rook::Response$new()
